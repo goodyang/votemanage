ab7675658e9fea1e90098321096f559c889c62da
goodyang <972750460@qq.com>
2016-01-26 00:08:06 +0800

    version 0.1

---
+++ votemanage/src/main/java/com/goodyang/votemanage/bean/Channel.java
@@ -0,0 +1,20 @@
+package com.goodyang.votemanage.bean;
+
+
+public class Channel {
+	private int channelID;		//Ƶ��ID
+	private String channelName;	//Ƶ�����
+	public int getChannelID() {
+		return channelID;
+	}
+	public void setChannelID(int channelID) {
+		this.channelID = channelID;
+	}
+	public String getChannelName() {
+		return channelName;
+	}
+	public void setChannelName(String channelName) {
+		this.channelName = channelName;
+	}
+	
+}
---
+++ votemanage/src/main/java/com/goodyang/votemanage/bean/Vote.java
@@ -0,0 +1,26 @@
+package com.goodyang.votemanage.bean;
+
+
+public class Vote {
+	private int voteID;		//ͶƱID
+	private String voteName;	//ͶƱ���
+	private int channelID;	//Ƶ��ID
+	public int getVoteID() {
+		return voteID;
+	}
+	public void setVoteID(int voteID) {
+		this.voteID = voteID;
+	}
+	public String getVoteName() {
+		return voteName;
+	}
+	public void setVoteName(String voteName) {
+		this.voteName = voteName;
+	}
+	public int getChannelID() {
+		return channelID;
+	}
+	public void setChannelID(int channelID) {
+		this.channelID = channelID;
+	}
+}
